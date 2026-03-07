cae1af530b5bacb9fd6e6469c47073cb198da631
CafeOS Dev <tchouhanjsm@gmail.com>
2026-03-07 22:57:23 +0530

    JWT added

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,75 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Mass Assignable Fields
+    |--------------------------------------------------------------------------
+    */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'pin_code',
+        'role',
+        'is_active'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Hidden Fields
+    |--------------------------------------------------------------------------
+    */
+
     protected $hidden = [
         'password',
-        'remember_token',
+        'pin_code',
+        'remember_token'
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    /*
+    |--------------------------------------------------------------------------
+    | Attribute Casting
+    |--------------------------------------------------------------------------
+    */
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'is_active' => 'boolean'
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | JWT Identifier
+    |--------------------------------------------------------------------------
+    */
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | JWT Custom Claims
+    |--------------------------------------------------------------------------
+    */
+
+    public function getJWTCustomClaims()
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'role' => $this->role,
+            'name' => $this->name
         ];
     }
 }
